877511a56ea58f88b197faf258bff3e92558f4d4
Christixxn <u24614794@tuks.co.za>
2025-05-26 19:39:08 +0200

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Practical_7
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // jQuery core library
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.7.0.min.js"));
+
+            // jQuery validation scripts explicitly included
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
+
+            // Modernizr script for feature detection
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-custom.js"));
+
+            // Bootstrap scripts bundle
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.min.js"));
+
+            // CSS bundle for Bootstrap and site styles
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"));
+
+            // Enable optimizations for bundling
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
